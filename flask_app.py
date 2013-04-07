b5792423daa579a92226daea3a70c5ce0aa36fe7
Harry <hjwp2@cantab.net>
2013-04-07 12:56:17 +0000

    Adding "site rebuilding" message...

--- flask_app.py
+++ flask_app.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    return "If you're seeing this, I'm probably rebuilding the site.  It should be back within seconds... If you just can't wait, go here: http://chimera.labs.oreilly.com/books/1234000000754"
 
