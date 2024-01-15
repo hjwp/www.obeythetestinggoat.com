015bf86fca6d78047b910682981e69a6d259abbe
Harry <hjwp2@cantab.net>
2024-01-15 22:40:45 +0000

    redirects

--- flask_app.py
+++ flask_app.py
@@ -12,43 +12,27 @@ def hello_world():
     )
 
 
-REDIRECTS = {
-    'appendix_II_Django_Class-Based_Views.html': 'appendix_Django_Class-Based_Views.html',
-    'appendix_VII_DjangoRestFramework.html': 'appendix_DjangoRestFramework.html',
-    'appendix_VI_rest_api.html': 'appendix_rest_api.html',
-    'appendix_V_bdd_tools.html': 'appendix_bdd.html',
-    'chapter_02.html': 'chapter_02_unittest.html',
-    'chapter_03.html': 'chapter_unit_test_first_view.html',
-    'chapter_04.html': 'chapter_philosophy_and_refactoring.html',
-    'chapter_05.html': 'chapter_post_and_database.html',
-    'chapter_06.html': 'chapter_explicit_waits_1.html',
-    'chapter_07.html': 'chapter_working_incrementally.html',
-    'chapter_08.html': 'chapter_prettification.html',
-    'chapter_09.html': 'chapter_manual_deployment.html',
-    'chapter_10.html': 'chapter_automate_deployment_with_fabric.html',
-    'chapter_11.html': 'chapter_database_layer_validation.html',
-    'chapter_12.html': 'chapter_simple_form.html',
-    'chapter_13.html': 'chapter_advanced_forms.html',
-    'chapter_14.html': 'chapter_javascript.html',
-    'chapter_15.html': 'chapter_deploying_validation.html',
-    'chapter_16.html': 'chapter_spiking_custom_auth.html',
-    'chapter_17.html': 'chapter_mocking.html',
-    'chapter_18.html': 'chapter_server_side_debugging.html',
-    'chapter_19.html': 'chapter_outside_in.html',
-    'chapter_20.html': 'chapter_purist_unit_tests.html',
-    'chapter_21.html': 'chapter_CI.html',
-    'chapter_22.html': 'chapter_page_pattern.html',
-    'chapter_23.html': 'chapter_hot_lava.html',
-}
 
+@app.route("/book/chapter_unit_test_first_view.html")
+def redirect_1():
+    return redirect("/book/chapter_03_unit_test_first_view.html", code=301)
 
-@app.route('/book/chapter_<n>.html')
-def chapter_redirector(n):
-    old_name = 'chapter_{n}.html'.format(n=n)
-    return redirect('/book/' + REDIRECTS[old_name], code=301)
+@app.route("/book/chapter_philosophy_and_refactoring.html")
+def redirect_2():
+    return redirect("/book/chapter_04_philosophy_and_refactoring.html", code=301)
 
-@app.route('/book/appendix_<thing>.html')
-def appendix_redirector(thing):
-    old_name = 'appendix_{thing}.html'.format(thing=thing)
-    return redirect('/book/' + REDIRECTS[old_name], code=301)
+@app.route("/book/chapter_post_and_database.html")
+def redirect_3():
+    return redirect("/book/chapter_05_post_and_database.html", code=301)
 
+@app.route("/book/chapter_explicit_waits_1.html")
+def redirect_4():
+    return redirect("/book/chapter_06_explicit_waits_1.html", code=301)
+
+@app.route("/book/chapter_working_incrementally.html")
+def redirect_5():
+    return redirect("/book/chapter_07_working_incrementally.html", code=301)
+
+@app.route("/book/chapter_prettification.html")
+def redirect_6():
+    return redirect("/book/chapter_08_prettification.html", code=301)
